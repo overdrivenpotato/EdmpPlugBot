a68cc4a69904fe535be93d7846a17d723357b680
Invincibear <regan.corey@gmail.com>
2014-03-09 20:12:01 -0600

    checkrepeatsong updates

--- src/bot.js
+++ src/bot.js
@@ -307,16 +307,16 @@ function checkRepeatSong(obj)
     }
     log("songs length: " + songs.length + "; list:: " + songs.join(","), log.visible);
 
-    if (songs.length == 4 && songs[1] == API.getMedia().cid && songs[2] == API.getMedia().cid && songs[3] == API.getMedia().cid) {
+    if (songs.length >= 4 && songs[1] == API.getMedia().cid && songs[2] == API.getMedia().cid && songs[3] == API.getMedia().cid) {
         API.moderateRemoveDJ(getId(API.getDJ()));
         skipDj();
         log("@" + API.getDJ() + ", you've already played that song before. Please play a different song.");
     } else {
-        if (songs.length == 3 && songs[1] == API.getMedia().cid && songs[2] == API.getMedia().cid) {
+        if (songs.length >= 3 && songs[1] == API.getMedia().cid && songs[2] == API.getMedia().cid) {
             skipDj();
             log("@" + API.getDJ() + ", you've already played that song twice before. Please play a different song or you will be removed from the DJ wait list.");
         } else {
-            if (songs.length == 2 && songs[1] == API.getMedia().cid) {
+            if (songs.length >= 2 && songs[1] == API.getMedia().cid) {
                 log("@" + API.getDJ() + ", you've already played that song thrice before. Please play a different song and rejoin the DJ wait list.");
             }
         }
