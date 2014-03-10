6779d2faa45d47a9c63299a1a568971d5ff494db
overdrivenpotato <marko.mijalkovic97@gmail.com>
2014-03-09 21:19:45 -0400

    Formatting

--- src/bot.js
+++ src/bot.js
@@ -71,7 +71,7 @@ function meetupReminder()
     if(meetupUrl.length > 0 && Date.now() - lastMeetupMessageTime > 600000)
     {
         lastMeetupMessageTime = Date.now();
-        chat("Make sure to " + upvotes[Math.round(Math.random() * upvotes.length)] + "the r/edmp thread at " + meetupUrl + "!");
+        chat("Make sure to " + upvotes[Math.round(Math.random() * upvotes.length)] + " the /r/edmp thread at " + meetupUrl + "!");
     }
 }
 
