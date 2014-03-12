2d6022b5582e77f6a14b91bdfe3f360728a90578
Invincibear <regan.corey@gmail.com>
2014-03-12 15:48:24 -0600

    added checkCustomPerm and tweaked command function to allow me to update the bot

--- src/bot.js
+++ src/bot.js
@@ -9,7 +9,7 @@ var version = "0.2.0";
 var trackAFKs = [];
 
 
-//API.on(API.WAIT_LIST_UPDATE, waitListUpdated);
+API.on(API.WAIT_LIST_UPDATE, waitListUpdated);
 API.on(API.DJ_ADVANCE, checkRepeatSong);
 API.on(API.CHAT, onChat);
 
@@ -147,7 +147,7 @@ function getPermLevel(username)
 }
 
 function checkCustomPerm(username) {
-    return (author == username) ? true : false;
+    return (author == username);
 }
 
 function getId(username) {
@@ -172,7 +172,7 @@ function getPosition(username) {
 
 // Alert upcoming users that their set is about to start when total users > if they're AFK
 function waitListUpdated (users) {
-    if (users.length >= 7) {
+    if (users.length >= 2) {
         log("@" + users[1].username + ", your set begins in ~" + getETA(users[1].username)+ " minutes", log.visible);
     }
 }
