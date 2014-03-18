97c6cbe78e5bfb8d12cf68e6a0c5d40c41851580
Invincibear <regan.corey@gmail.com>
2014-03-18 11:35:16 -0600

    troubleshooting onJoin

--- src/bot.js
+++ src/bot.js
@@ -341,7 +341,9 @@ function onDJAdvance(obj) {
 }
 
 
-function onJoin(newuser) {// greet new user after a short delay to ensure they receive the message
+function onJoin(user) {// greet new user after a short delay to ensure they receive the message
+    log(user, log.info);
+    var newuser = user;
     log(newuser, log.info);
     setTimeout('log("Welcome @" + newuser.username + "! Type !help for more information and a list of available commands.", log.visible);', 2000);
 }
