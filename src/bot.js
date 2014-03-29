0941570f55373db40e2bd3f89c769fbf4e91cf70
overdrivenpotato <marko.mijalkovic97@gmail.com>
2014-03-29 19:57:07 -0400

    Changed to v1.7.1

--- src/bot.js
+++ src/bot.js
@@ -33,7 +33,7 @@ var blackJackEnabled= true;//(curdate.getDay() != 3 && curdate.getDay() != 6);//
 var ReminderEnabled = false;//(curdate.getDay() == 3 || curdate.getDay() == 6);// disable reminder on non-meet days to prevent spam
 var GreetingEnabled = (curdate.getDay() != 3 && curdate.getDay() != 6);// disable by default on meet-up days
 
-var version   = "0.7";
+var version   = "0.7.1";
 var meetupUrl = "http://reddit.com/r/edmproduction/";
 
 var trackAFKs        = [];// format: array[0=>username, 1=>userID, 2=>time of last msg, 3=>message data/txt, 4=bool warned or not]
