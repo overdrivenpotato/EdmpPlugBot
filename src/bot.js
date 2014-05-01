26639638c366e0eaa0178d5ed5cea5d19080d762
Invincibear <regan.corey@gmail.com>
2014-05-01 02:34:19 -0600

    investigating rtd

--- src/bot.js
+++ src/bot.js
@@ -537,16 +537,16 @@ function analyzeSongHistory() {
 
 
 function rollTheDice(author) {
-    if((API.getWaitList().length - (getPosition(author) + 1)) < 3 ) {// Must not be [3rd last - last]
-        log("@" + author + ", you can't roll if you're fresh on the DJ wait list, wait a few songs or get help by typing !addiction", log.visible);
-        return;
-    } else if(getPosition(author) == -1) {
-        log("@" + author + ", you're already DJing, you can't move positions!", log.visible);
-        return;
-    } else if(getPosition(author) == 0) {
-        log("@" + author + ", you're already the next to DJ, type !addiction for help with your problem.", log.visible);
-        return;
-    }
+//    if((API.getWaitList().length - (getPosition(author) + 1)) < 3 ) {// Must not be [3rd last - last]
+//        log("@" + author + ", you can't roll if you're fresh on the DJ wait list, wait a few songs or get help by typing !addiction", log.visible);
+//        return;
+//    } else if(getPosition(author) == -1) {
+//        log("@" + author + ", you're already DJing, you can't move positions!", log.visible);
+//        return;
+//    } else if(getPosition(author) == 0) {
+//       log("@" + author + ", you're already the next to DJ, type !addiction for help with your problem.", log.visible);
+//        return;
+//    }
 
     var x = Math.floor(Math.random() * ((6 - 1) + 1) + 1);
     var y = Math.floor(Math.random() * ((6 - 1) + 1) + 1);
