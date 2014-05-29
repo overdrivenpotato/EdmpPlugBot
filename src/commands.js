4b632adfb07bc073f4fdc782372e0171f268e2f6
Invincibear <regan.corey@gmail.com>
2014-05-29 07:12:54 -0600

    !admins fix

--- src/commands.js
+++ src/commands.js
@@ -185,7 +185,7 @@ var commands = [
         var realAdmins  = [];
 
         for(var i = 0; i < admins.length; i++) {
-            logtext = (admins[i].id != botID && admins[i].permissions >= API.ROLE.BOUNCER) ? logtext + (admins[i].username + ((i == (admins.length - 1)) ? "" : ", ")) : logtext + "";
+            logtext = (admins[i].id != botID && admins[i].permission >= API.ROLE.BOUNCER) ? logtext + (admins[i].username + ((i == (admins.length - 1)) ? "" : ", ")) : logtext + "";
 
             if(admins[i].permissions >= API.ROLE.BOUNCER) {
                 realAdmins.push(admins[i]);
