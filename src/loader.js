f2b916b2df3af23b9d670a8c1ee3335d0cb41c10
overdrivenpotato <marko.mijalkovic97@gmail.com>
2014-10-01 20:08:07 -0400

    Fixed MIME type issues when loading

--- src/loader.js
+++ src/loader.js
@@ -5,17 +5,17 @@
  * Time: 10:34 PM
  */
 var scripts = [
-    "https://empirestorage.ca/edmp?f=commands.js",
-    "https://empirestorage.ca/edmp?f=cron.js",
-    "https://empirestorage.ca/edmp?f=lottery.js",
-    "https://empirestorage.ca/edmp?f=blackjack.js",
-    "https://empirestorage.ca/edmp?f=afkcheck.js",
-    "https://empirestorage.ca/edmp?f=handlers.js",
-    "https://empirestorage.ca/edmp?f=bot.js"];
+    "https://rawgit.com/overdrivenpotato/EdmpPlugBot/master/src/commands.js",
+    "https://rawgit.com/overdrivenpotato/EdmpPlugBot/master/src/cron.js",
+    "https://rawgit.com/overdrivenpotato/EdmpPlugBot/master/src/lottery.js",
+    "https://rawgit.com/overdrivenpotato/EdmpPlugBot/master/src/blackjack.js",
+    "https://rawgit.com/overdrivenpotato/EdmpPlugBot/master/src/afkcheck.js",
+    "https://rawgit.com/overdrivenpotato/EdmpPlugBot/master/src/handlers.js",
+    "https://rawgit.com/overdrivenpotato/EdmpPlugBot/master/src/bot.js"];
 
 
 function updateBot() {
-    setTimeout(function(){$.getScript("https://empirestorage.ca/edmp?f=loader.js");}, 2000);
+    setTimeout(function(){$.getScript("https://rawgit.com/overdrivenpotato/EdmpPlugBot/master/src/loader.js");}, 2000);
 }
 
 
