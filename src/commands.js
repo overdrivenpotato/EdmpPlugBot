9e16bed6fa20915f8ed4270f5e97fa312b33b439
Corey <regan.corey@gmail.com>
2015-01-25 01:34:45 -0700

    added !smite

--- src/commands.js
+++ src/commands.js
@@ -229,7 +229,7 @@ var commands = [
             log("@" + author + " please use in the form of: !smite @" + author);
             return;
         }
-
+log(";name: "+getId(args[1].substr(1))+"; id:"+getId(args[1].substr(1)) + "args: "+args, log.info);
         API.API.moderateRemoveDJ(getId(args[1].substr(1)));
     }, API.ROLE.BOUNCER),
 
