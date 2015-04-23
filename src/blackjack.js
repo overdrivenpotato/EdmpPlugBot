00dc4ed5bdf76389be5462e17f0ace9ed305a926
Corey <regan.corey@gmail.com>
2015-04-22 23:11:27 -0600

    removed auto-deleting blackjack chat calls

--- src/blackjack.js
+++ src/blackjack.js
@@ -385,7 +385,7 @@ function checkBlackJackOutput(chatID, message) {
     } else {
         if(lastBlackJack != "") {
 log("!!!!!!!!!!!!!!!!!!! API.moderateDeleteChat(lastBlackJack); API.moderateDeleteChat(" + lastBlackJack + ");", log.info);
-            API.moderateDeleteChat(lastBlackJack);
+//            API.moderateDeleteChat(lastBlackJack);
         }
 
         lastBlackJack = chatID;
